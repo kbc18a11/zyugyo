38f0eddff6a90d5b40951ff8ae79af7d91207678
wadanaoumi <wada121400@gmail.com>
2021-02-12 12:21:48 +0900

    てすとしゅせい

--- tests/systemtest.js
+++ tests/systemtest.js
@@ -8,8 +8,6 @@ const cheerio = require('cheerio');
 describe('Web システムテスト', async function () {
     it('Access with GET Method return 200', async function () {
         request.get({ url: 'http://localhost:3000' }, function (err, response, body) {
-            console.log(response.statusCode);
-
             assert.equal(response.statusCode, 200);
         });
     });
